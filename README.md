06bdffd1705d1eaaa4782504c66b03eddbc1d3f2
Melina Boedecker <melinamboedecker@gmail.com>
2021-05-23 17:47:39 -0700

    cleaned files and added main folder

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-# rytrutr
-[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0) 
+# README file Generator
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT) 
 ## Description: 
-This is a command line tool using node.js and inquirer.js to allow users to generate a quality readme.md file by answering the questions presented in the command line.
+This is a command line app using node.js and inquirer.js for users to generate a quality README file for their applications by answering the questions presented in the command line.  This tool will allow users to easily generate their README files in one easy step without forgetting to include pertinent inforamation. 
 ## Table of Contents: 
 * [Installation](#installation)
 * [Usage](#usage) 
@@ -10,15 +10,19 @@ This is a command line tool using node.js and inquirer.js to allow users to gene
 * [Tests](#tests) 
 * [Questions](#questions) 
 ## Installation: 
+Run npm install to install dependencies.
 ## Usage: 
-rthg
-![Screenshot](fgnd)
-## Link: 
-et
+Run node index.js in the command line to start the readme generator, then answer the prompts. 
+
+![Screenshot](main/2readmegeneratorpractice.gif)
+
 ## License: 
-Apache
-## Contributing
-eyt
+Licensed under the MIT license.
+## Contributing:
+Feel free to contact me to contribute to this project. My contact information is listed below. 
 ## Tests: 
+There are no tests for this applicaion at this time.
 ## Questions
-eyt
+GitHub: [melinamboedecker](https://github.com/melinamboedecker) 
+
+If you have any additional questions, please contact me at melinamboedecker@gmail.com
--- main/index.js
+++ main/index.js
@@ -1,12 +1,9 @@
-// TODO: Include packages needed for this application
+// packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
 
-// const requireEmail = (value) => {
-//     if (/\w/.test)
-// }
 
-// TODO: Create an array of questions for user input
+//array of questions for user input
 const questions = [
     {
         type: 'input',
@@ -27,11 +24,6 @@ const questions = [
         type: 'input',
         name: 'usage',
         message: 'Enter usage instructions and examples for use',
-    },
-    {
-        type: 'input',
-        name: 'screenshot',
-        message: 'add path for screenshot',
     },
     {
         type: 'input',
@@ -74,35 +66,47 @@ const questions = [
     }
 ];
 
+//variablefor license badge
 var licenseBadge;
+//variable for license link
 var licenseLink;
 
+//fuction to get the correct badge and link for chosen license
 function getBadge (chosenLicense) {
     if (chosenLicense === 'MIT') {
         licenseBadge = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
-        licenseLink = '## License: \n Licensed under the [MIT](https://opensource.org/licenses/MIT) license.';
+        licenseLink = 'Licensed under the [MIT](https://opensource.org/licenses/MIT) license.';
     } else {
         if (chosenLicense === 'Apache') {
             licenseBadge = '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
-            licenseLink = '## License: \n Licensed under the [Apache](https://opensource.org/licenses/Apache-2.0) license.';
+            licenseLink = 'Licensed under the [Apache](https://opensource.org/licenses/Apache-2.0) license.';
         } else {
             if (chosenLicense === 'GPL') {
                 licenseBadge = '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
-                licenseLink = '## License: \n Licensed under the [GPL](https://www.gnu.org/licenses/gpl-3.0) license.';
+                licenseLink = 'Licensed under the [GPL](https://www.gnu.org/licenses/gpl-3.0) license.';
             } else {
                 licenseBadge = ''
-                licenseLink = ''
+                licenseLink = 'none'
             }
         }
     }
 }
 
+//variable and function for adding link to deployed page if entered
+var deployedPage;
+function getAppLink (link) {
+    if (!link) {
+        deployedPage = '';
+    } else {
+        deployedPage = `\n[Click here for deployed page](`+link+')'
+    }
+}
 
 
-
-// TODO: Create a function to write README file
+//  function to write README file with data per users input
 function writeToFile(fileName, data) { 
     getBadge(data.license)
+    getAppLink(data.link)
     fs.writeFile(fileName, 
         '# ' +data.title+ '\n'+
         `${licenseBadge} \n`+
@@ -116,12 +120,13 @@ function writeToFile(fileName, data) {
         '* [Tests](#tests) \n'+
         '* [Questions](#questions) \n'+
         '## Installation: \n'+
+        data.installation+'\n'+
         '## Usage: \n'+
-        data.usage + '\n'+
-        '![Screenshot]('+data.screenshot+')\n'+
-        '[Deployed page](#'+data.link+'\n'+
+        data.usage+'\n'+
+        
+        `\n${deployedPage}\n`+
        
-     
+        '## License: \n'+
         `${licenseLink} \n`+
         
         '## Contributing: \n'+
@@ -131,20 +136,20 @@ function writeToFile(fileName, data) {
         data.tests+'\n'+
         
         '## Questions: \n'+
-        'GitHub: ['+data.githubusername+'](#https://github.com/'+data.githubusername+') \n'+
+        'GitHub: ['+data.githubusername+'](https://github.com/'+data.githubusername+') \n'+
         '\n'+
         'If you have any additional questions, please contact me at '+data.email+'\n'
 
         , (err) => err ? console.log(err) : console.log('Readme file created!'));
 }
 
-// TODO: Create a function to initialize app
+//function to initialize app
 function init() {
     const inquirer = require('inquirer');
     inquirer.prompt(questions)
     .then((data) => {
         console.log(data);
-        writeToFile(`${data.title}_readme.md`, data);
+        writeToFile(`README.md`, data);
     })
     
 }
